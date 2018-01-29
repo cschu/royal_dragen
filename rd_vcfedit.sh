900698a38168cb4c3a46bd61bed63a52b78adaab
Christian Schudoma <cschu1981@gmail.com>
2018-01-29 11:16:21 +0000

    Update rd_vcfedit.sh
    Added non-slurm option.

--- rd_vcfedit.sh
+++ rd_vcfedit.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -e
 source vcftools-0.1.13
 
-vcfin=$(sed -n "$SLURM_ARRAY_TASK_ID"p $1)
+# uncomment next (and comment next next) line to switch between slurm array and single job
+# vcfin=$(sed -n "$SLURM_ARRAY_TASK_ID"p $1)
+vcfin=$1
+
 vcfout=$(dirname $vcfin)/$(basename $vcfin .gz).nod.gz
 gzip -dc $vcfin | grep -v '##DRAGEN' | bgzip -c > $vcfout && tabix -p vcf $vcfout;
 
