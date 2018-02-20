61e676faff1e7dbd5936678c503a493132745fa8
Christian Schudoma (cschu) <christian.schudoma@earlham.ac.uk>
2018-02-20 14:59:23 +0000

    rd_vcfedit.sh now removes all unnecessary headerlines

--- rd_vcfedit.sh
+++ rd_vcfedit.sh
@@ -6,5 +6,5 @@ source vcftools-0.1.13
 vcfin=$1
 
 vcfout=$(dirname $vcfin)/$(basename $vcfin .gz).nod.gz
-gzip -dc $vcfin | grep -v '##DRAGEN' | bgzip -c > $vcfout && tabix -p vcf $vcfout;
+gzip -dc $vcfin | awk '/^#/ { if (NR==1 || substr($0,2,1) != "#") print $0; next;} {print $0;}' | bgzip -c > $vcfout && tabix -p vcf $vcfout;
 
